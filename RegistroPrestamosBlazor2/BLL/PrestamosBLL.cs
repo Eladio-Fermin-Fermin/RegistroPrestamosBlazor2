66d15a07c94287aed4ffbe89ad1a40701d6fa9c8
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-01-31 12:44:42 -0400

    Se crearon las clase PersonasBLL, PrestamosBLL y Contexto.

---
+++ RegistroPrestamosBlazor2/BLL/PrestamosBLL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPrestamosBlazor2.BLL
+{
+    public class PrestamosBLL
+    {
+    }
+}
---
+++ RegistroPrestamosBlazor2/DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPrestamosBlazor2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPrestamosBlazor2.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+        //public DbSet<Prestamos> Prestamos { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
