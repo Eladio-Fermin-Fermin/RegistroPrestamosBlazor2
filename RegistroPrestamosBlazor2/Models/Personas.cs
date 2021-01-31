2ec08a6f5bd018c10b1974585cd11c3bdf837a14
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-01-31 12:38:55 -0400

    Se crearon las clases Personas y Prestamos.

---
+++ RegistroPrestamosBlazor2/Models/Personas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPrestamosBlazor2.Models
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir un Nombre.")]
+        public string Nombres { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir un Número Telefónico .")]
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir una Cédula.")]
+        public string Cedula { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir una dirección.")]
+        public string Direccion { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir una fecha de nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
---
+++ RegistroPrestamosBlazor2/Models/Prestamos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPrestamosBlazor2.Models
+{
+    public class Prestamos
+    {
+        [Key]
+        public int PrestamoId { get; set; }
+        //[Required(ErrorMessage = "Es obligatorio introducir la fecha")]
+        public DateTime Fecha { get; set; }
+        //[Required(ErrorMessage = "Es obligatorio introducir una PersonaId")]
+        public int PersonaId { get; set; }
+        //[Required(ErrorMessage = "Es obligatorio introducir un Concepto")]
+        public float Concepto { get; set; }
+        //[Required(ErrorMessage = "Es obligatorio introducir un Monto")]
+        public float Monto { get; set; }
+        public float Balance { get; set; }
+    }
+}
